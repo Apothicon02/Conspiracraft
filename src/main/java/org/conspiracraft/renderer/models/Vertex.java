4edfd3a37a84a973aab851fef4caaec4ea9c648f
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2026-04-03 16:33:54 -0400

    - Added vertex sharing.

--- src/main/java/org/conspiracraft/renderer/models/Vertex.java
+++ src/main/java/org/conspiracraft/renderer/models/Vertex.java
@@ -1,13 +1,14 @@
 package org.conspiracraft.renderer.models;
 
+import org.joml.Vector2f;
 import org.joml.Vector3f;
 import org.lwjgl.vulkan.VkVertexInputAttributeDescription;
 import org.lwjgl.vulkan.VkVertexInputBindingDescription;
 
 import static org.lwjgl.vulkan.VK10.*;
 
-public record Vertex(Vector3f pos, Vector3f normal) {
-    public static final int SIZE = 4*(3+3);
+public record Vertex(Vector3f pos, Vector3f normal, Vector2f uvs) {
+    public static final int SIZE = 4*(3+3+2);
     public static VkVertexInputBindingDescription.Buffer getBindingDescription() {
         VkVertexInputBindingDescription.Buffer bindingDescription = VkVertexInputBindingDescription.calloc(1)
                 .binding(0)
@@ -16,17 +17,22 @@ public record Vertex(Vector3f pos, Vector3f normal) {
         return bindingDescription;
     }
     public static VkVertexInputAttributeDescription.Buffer getAttributeDescriptions() {
-        VkVertexInputAttributeDescription.Buffer attributeDescriptions = VkVertexInputAttributeDescription.calloc(2);
-        attributeDescriptions.get(0)
+        VkVertexInputAttributeDescription.Buffer attributeDescriptions = VkVertexInputAttributeDescription.calloc(3);
+        attributeDescriptions.get(0) //pos
                 .location(0)
                 .binding(0)
                 .format(VK_FORMAT_R32G32B32_SFLOAT)
                 .offset(0);
-        attributeDescriptions.get(1)
+        attributeDescriptions.get(1) //normal
                 .location(1)
                 .binding(0)
                 .format(VK_FORMAT_R32G32B32_SFLOAT)
                 .offset(12);
+        attributeDescriptions.get(2) //uv
+                .location(2)
+                .binding(0)
+                .format(VK_FORMAT_R32G32_SFLOAT)
+                .offset(24);
         return attributeDescriptions;
     }
 }
